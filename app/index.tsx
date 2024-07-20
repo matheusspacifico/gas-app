bbbcd2c4c7f2ce16528101adbf859e5eb135091b
Matheus <matheuspacifico44@gmail.com>
2024-07-20 00:43:17 -0300

    added "calcular direto" button and more basic styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,20 @@
-import { Text, View, SafeAreaView, StyleSheet } from "react-native";
+import { Text, View, SafeAreaView, StyleSheet, TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 export default function Index() {
   return (
     <SafeAreaView style={styles.SafeAreaView}>
-      <View style={styles.NovoCarro}>
+
+      <TouchableOpacity style={styles.NovoCarro} onPress={() => console.log("novo carro")}>
         <Ionicons name="add-circle-sharp" size={50} color={"black"} style={styles.NovoCarroIcon}/>
         <Text style={styles.NovoCarroText}>NOVO CARRO</Text>
-      </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.CalcularDireto} onPress={() => console.log("calcular direto")}>
+        <Text style={styles.CalcularDiretoText}>Calcular Direto</Text>
+      </TouchableOpacity>
+
+
     </SafeAreaView>
   );
 }
@@ -33,4 +40,32 @@ const styles = StyleSheet.create({
     fontSize: 25,
     fontWeight: "bold",
   },
+
+  CalcularDireto: {
+    position: 'absolute',
+    bottom: 20,
+    left: 0,
+    right: 0,
+    marginLeft: 20,
+    marginRight: 20,
+    alignSelf: 'center',
+    alignItems: 'center',
+    backgroundColor: "#E5E7EB",
+    padding: 20,
+    borderRadius: 5,
+
+    // SOMBRA PRO IOS
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
+    // SOMBRA PRO IOS
+
+    elevation: 5, // SOMBRA PRO ANDROID
+  },
+
+  CalcularDiretoText: {
+    fontSize: 20,
+    fontWeight: "bold",
+  }
 });
